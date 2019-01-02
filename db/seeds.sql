935e40e0c90caaf7b4ae30ddf28f80fe9915362b
WagnerAndy <wagner.andrew.james@gmail.com>
2019-01-02 15:12:19 -0700

    File structure in place - need front end

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheddar');
+INSERT INTO burgers (burger_name) VALUES ('Double Patty');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Barbeque', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalepeno', false);
