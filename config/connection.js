a19026c4e9e1ec03dd7982bf32e4daf614278315
WagnerAndy <wagner.andrew.james@gmail.com>
2019-01-06 11:35:59 -0700

    Added JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+
+}
 
 connection.connect(function(err) {
   if (err) {
@@ -18,3 +26,35 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
